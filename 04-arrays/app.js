9c28433998b074421898249fad7782aa0937b30d
Jeanine Schoessler <jeanine.mt@gmail.com>
2018-03-10 20:59:06 -0700

    begin building data for array cardio 4

---
+++ 04-arrays/app.js
@@ -0,0 +1,14 @@
+
+const inventors = [
+  {"first":"Mara","last":"Hawkins","year":1950,"passed":1978},
+  {"first":"Akeem","last":"Wolf","year":1959,"passed":1961},
+  {"first":"Yuri","last":"Gay","year":1921,"passed":1963},
+  {"first":"Deacon","last":"Soto","year":1933,"passed":1967},
+  {"first":"Louis","last":"Scott","year":1937,"passed":1962},
+  {"first":"Macaulay","last":"Mcdonald","year":1920,"passed":1963}
+];
+
+const people = [
+  "Wolf, Akeem", "Gay, Yuri", "Soto, Deacon", "Scott, Louis", "Mcdonald, Macaulay", "Hawkins, Mara", 
+  "Page, Tanya R.", "Justice, Amela P.", "Sherman, Paula I.", "Cortez, Jillian K.", "Orr, Leroy H.", "Reid, Kathleen N.", "Chan, Branden Y.", "Case, Todd Q.", "Ferguson, Trevor A.", "Ray, Jerome B.", "Craft, Kylynn Z.", "Morton, Tamekah E.", "Marks, Kato P.", "Greer, Jesse J.", "Cannon, Stephen E.", "Mcdonald, Yoko J.", "Brooks, Sawyer Y.", "Munoz, Shaine F.", "Merritt, Savannah E.", "Montoya, Stephen B.", "Arnold, Vivian P.", "Bernard, Bruce Z.", "Marks, Laura Q.", "Mccullough, Kitra B.", "Newman, Ariana L.", "Bryant, Basil A.", "Manning, Idona T.", "Salas, James K.", "Finch, Stewart V.", "Farrell, Akeem A.", "Williams, Joel S.", "Good, Hammett J.", "Little, Xantha M.", "Harding, Audra V.", "Pate, Yoshi Q.", "Bean, Catherine Q.", "Byrd, Jocelyn G.", "White, Tara A.", "Baird, Amery V.", "Rhodes, Mariko E.", "Elliott, Zeus J.", "Bernard, Giselle N.", "Herrera, Todd B.", "Baird, Quentin Q.", "Navarro, Xaviera D.", "Golden, Keiko M.", "Callahan, Edan D.", "Perez, Alfonso G.", "Prince, Nyssa G.", "Horne, Hayden J.", "Buck, Trevor P.", "Maxwell, Burton S.", "Sharpe, Thane X.", "York, Tyrone T.", "Pittman, Elton R.", "Chapman, Fredericka Q", "Mcintosh, Aubrey J.", "Dillard, Ryder B.", "Everett, Kerry Z.", "Baird, Ferdinand E.", "Gregory, Brittany N.", "Moreno, Avye N.", "Mercado, Kylynn B.", "Harrington, Calvin F.", "Gibbs, Olivia J.", "Norris, Marshall I.", "Larson, Renee X.", "Wade, Molly Q.", "Contreras, Hyacinth S", "Small, Vaughan K.", "Peters, Tyrone J.", "Oneill, Lars T.", "Hammond, Nita V.", "York, Shaine X.", "Freeman, Conan N.", "Buckner, Kenneth C.", "Bird, Glenna E.", "Gutierrez, Gail J.", "Snider, Kiara G.", "Jordan, Ivan L.", "Puckett, Jack P.", "Andrews, Jordan V.", "Faulkner, Kibo T.", "Shepherd, Sawyer D.", "Mcmahon, Malachi W.", "Patterson, Alma D.", "Buchanan, Jena I.", "Cooke, Briar C.", "Mcknight, Holmes H.", "Dickson, Tasha N.", "Gregory, Veronica Z.", "Rutledge, Kelsie K.", "Cole, Oprah I.", "Giles, Herrod K.", "Rollins, Haviva G.", "Page, Alice N.", "Manning, Jaime L.", "Wilson, Kelsey M.", "Norris, Jessamine Z.", "Huber, Uriel R"
+];
